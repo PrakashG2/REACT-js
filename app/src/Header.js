ca6c58222c19938fb83554e824ad81e12bbeba1a
PrakashG2 <dhanaprakashofficial@gmail.com>
2023-06-03 16:08:45 +0530

    add subtract button

--- app/src/Header.js
+++ app/src/Header.js
@@ -1,15 +1,26 @@
 import { React } from "react";
+import { useState } from "react";
 
 export const Header = () => {
-  function handleClick(e){
-    return(
-      console.log(e.target.innerText)
-    )
+  function handleClick(e) {
+    return console.log(e.target.innerText);
   }
+
+  const [count, setCount] = useState(99);
+
+  function handleAddition() {
+    setCount(count + 1)
+  }
+
+  function handleSubtraction() {
+    setCount(count - 1);
+  }
+
   return (
     <>
-    <h1>hello world !</h1>
-    <button onClick={(e) => handleClick(e)}>click me</button>
+      <button onClick={() => handleAddition()}>+</button>
+      <span>{count}</span>
+      <button onClick={() => handleSubtraction()}>-</button>
     </>
-  )
-}
+  );
+};
